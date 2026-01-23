4fb44ce450b97a26b2e2cd878d97596e67b6a8cd
DARSX <developerdars@gmail.com>
2026-01-23 14:25:25 -0400

    Reestructuración del funcionamiento y migración a .NET 8
    Migración del proyecto a .NET 8.0 con soporte para Windows Forms,
    permitiendo la selección de archivos mediante OpenFileDialog.
    
    Integración de la librería EPPlus para lectura y validación de archivos
    Excel (.xlsx).
    
    Implementación de un menú interactivo, validación de existencia y
    contenido de archivos (.csv/.xlsx) y registro de eventos en archivo log.
    
    Actualización de archivos de configuración, dependencias y .gitignore.

--- ServicoLecturaDocumentos/obj/Debug/net8.0-windows/ServicoLecturaDocumentos.AssemblyInfo.cs
+++ ServicoLecturaDocumentos/obj/Debug/net8.0-windows/ServicoLecturaDocumentos.AssemblyInfo.cs
@@ -18,6 +18,8 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyProductAttribute("ServicoLecturaDocumentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServicoLecturaDocumentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
